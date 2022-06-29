2d518e35170a03f52678eb1425f0f734a9b755b2
derahcnwosu <derahcnwosu@gmail.com>
2022-06-29 22:53:12 +0100

    width adjusted on expert img

--- src/components/shared/Expert.jsx
+++ src/components/shared/Expert.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Expert({expert_name, expert_role, pic}) {
   return (
     <div className="expert">
-        <div className="expert-img" style={{background: `url(${pic}) no-repeat center center/cover`,height: '300px', width: '250px'}}></div>
+        <div className="expert-img" style={{background: `url(${pic}) no-repeat center center/cover`}}></div>
         <div className="expert-info">
             <h2>{expert_name}</h2>
             <p>{expert_role}</p>
